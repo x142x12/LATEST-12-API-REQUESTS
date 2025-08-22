c0ea9d8928f7c8e72a64787aca49fcb1b81f76bd
x142x12 <azfar_asif@digimarkdevelopers.com>
2025-08-22 17:08:12 +0500

    Add files via upload

---
+++ TodoApp/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Depends, HTTPException
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()    
+
+@app.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException() 
+
+def http_exception():
+    return HTTPException(status_code = 404, detail="Todo not found")       
+
---
+++ TodoApp/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, String, Integer
+from database import Base
+
+class Todos(Base):
+    __tablename__ = "todos"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(String)
+    complete = Column(Boolean, default=False)
+    
+    
